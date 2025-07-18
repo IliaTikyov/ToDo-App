ed26c0395364f10d2e15af208956fd2e29bc7c8f
IliaTikyov <iliatikiov123@gmail.com>
2025-07-18 19:15:13 +0300

    Creating The Header Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center mt-6">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-2xl font-bold text-amber-50">ToDo App</h1>
+    </header>
+  );
+};
+
+export default Header;
